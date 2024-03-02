dc6e173a6793007a8094a622d55ce06b67a6f686
ColdWindScholar <3590361911@qq.com>
2024-03-02 12:30:11 +0800

    remove  useless codes

--- pio/src/main/java/com/projectkr/shell/MainActivity.kt
+++ pio/src/main/java/com/projectkr/shell/MainActivity.kt
@@ -271,7 +271,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main, menu)
 
-        menu.findItem(R.id.action_graph).isVisible = (main_tabhost_cpu.visibility == View.VISIBLE)
+
 
         return true
     }
